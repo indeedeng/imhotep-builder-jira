52474a71c218d8aede655a679fb43ae06dcc939e
Shunsuke Oono <soono@indeed.com>
2016-09-22 16:10:14 -0500

    fix config

--- src/java/com/indeed/skeleton/index/builder/jiraaction/IssuesAPICaller.java
+++ src/java/com/indeed/skeleton/index/builder/jiraaction/IssuesAPICaller.java
@@ -24,7 +24,7 @@ public class IssuesAPICaller {
     // For Pagination
     //
 
-    private final int numPerPage = 10; // Max number of issues per page
+    private final int numPerPage = 5; // Max number of issues per page
     private int page = 0; // Current Page
     private int numTotal = -1; // Max number of issues per page
 
