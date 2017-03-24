9ccc411dc3fd681e9806b816cad19c524b567883
Kevin Binswanger <kbinswanger@indeed.com>
2017-03-24 15:50:38 -0500

    REDACTED-46: Remove the backfill concepts
    We don't use these things anymore, so...

--- src/java/com/indeed/skeleton/index/builder/jiraaction/JiraActionIndexBuilderCommandLineTool.java
+++ src/java/com/indeed/skeleton/index/builder/jiraaction/JiraActionIndexBuilderCommandLineTool.java
@@ -67,17 +67,11 @@ public class JiraActionIndexBuilderCommandLineTool implements CommandLineTool {
                     .isRequired()
                     .hasArg()
                     .withDescription("Number of issues to retrieve in each batch")
-                    .create("jiraBatchSize"))
-         .addOption(OptionBuilder
-                    .isRequired(false)
-                    .hasArg()
-                    .withDescription("Do extra work if we're trying to backfill")
-                    .create("backfill"));
+                    .create("jiraBatchSize"));
 
         final String startDate;
         final String endDate;
         final int jiraBatchSize;
-        final boolean backfill;
         final CommandLineParser parser = new GnuParser();
         final CommandLine commandLineArgs;
         try {
@@ -85,8 +79,6 @@ public class JiraActionIndexBuilderCommandLineTool implements CommandLineTool {
             startDate = commandLineArgs.getOptionValue("start");
             endDate = commandLineArgs.getOptionValue("end");
             jiraBatchSize = Integer.parseInt(commandLineArgs.getOptionValue("jiraBatchSize"));
-            backfill = Boolean.parseBoolean(commandLineArgs.getOptionValue("backfill"));
-
         } catch (final ParseException e) {
             log.error("Threw an exception trying to run the index builder", e);
             System.exit(-1);
@@ -95,7 +87,7 @@ public class JiraActionIndexBuilderCommandLineTool implements CommandLineTool {
 
         final JiraActionIndexBuilderConfig indexBuilderConfig = new JiraActionIndexBuilderConfig(jiraUsername,
                 jiraPassword, jiraBaseUrl, jiraFields, jiraExpand, jiraProject, iuploadUrl, startDate, endDate,
-                jiraBatchSize, backfill);
+                jiraBatchSize);
         indexBuilder = new JiraActionIndexBuilder(indexBuilderConfig);
     }
 
