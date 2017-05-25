2bae3c7857781334cbe16ae8d6805de48139c399
Kevin Binswanger <kbinswanger@indeed.com>
2017-05-25 11:49:34 -0500

    REDACTED-64: Add usernames

--- src/java/com/indeed/jiraactions/api/response/issue/fields/Field.java
+++ src/java/com/indeed/jiraactions/api/response/issue/fields/Field.java
@@ -21,7 +21,6 @@ public class Field {
     public User creator;
     public Issuetype issuetype;
     public Project project;
-    public String projectkey;
     public User reporter;
     public Resolution resolution;
     public Status status;
@@ -61,15 +60,18 @@ public class Field {
     public String getStringValue(final String attribute) throws Exception {
         switch (attribute) {
             case "assignee": return assignee == null ? "" : assignee.displayName;
+            case "assigneeusername": return assignee == null ? "" : assignee.key;
             case "creator": return creator == null ? "" : creator.displayName;
             case "issuetype": return issuetype == null ? "" : issuetype.name;
             case "project": return project == null ? "" : project.name;
             case "projectkey": return project == null ? "" : project.key;
             case "reporter": return reporter == null ? "" : reporter.displayName;
+            case "reporterusername": return reporter == null ? "" : reporter.key;
             case "resolution": return resolution == null? "" : resolution.name;
             case "status": return status == null? "" : status.name;
             case "summary": return summary;
             case "verifier": return verifier == null? "" : verifier.displayName;
+            case "verifierusername": return verifier == null? "" : verifier.key;
             case "category": {
                 final ProjectCategory category = project == null
                         ? null
