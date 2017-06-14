12f2007417e699a2cac427200b56b830a6116be9
Kevin Binswanger <kbinswanger@indeed.com>
2017-06-14 14:55:28 -0500

    REDACTED-100: Fix tests for direct cause

--- test/java/com/indeed/jiraactions/api/response/issue/fields/TestDirectCause.java
+++ test/java/com/indeed/jiraactions/api/response/issue/fields/TestDirectCause.java
@@ -7,9 +7,20 @@ public class TestDirectCause {
     @Test
     public void testToString() {
         final DirectCause dc = new DirectCause();
-        Assert.assertNull(dc.toString());
-
         dc.value = "value";
         Assert.assertEquals("value", dc.toString());
+    }
+
+    @Test
+    public void testChildToString() {
+        final DirectCause dc = new DirectCause();
+        dc.value = "parent";
+
+        final DirectCause child = new DirectCause();
+        child.value = "child";
+        dc.child = child;
+
+        Assert.assertEquals("child", child.toString());
+        Assert.assertEquals("parent - child", dc.toString());
     }
 }
