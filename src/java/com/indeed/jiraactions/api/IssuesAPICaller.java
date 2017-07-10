1d9cebf0fa78c01defd7e3ed7bf0d3379e84996f
Kevin Binswanger <kbinswanger@indeed.com>
2017-07-10 17:56:30 -0500

    Don't commit those changes to the api caller

--- src/java/com/indeed/jiraactions/api/IssuesAPICaller.java
+++ src/java/com/indeed/jiraactions/api/IssuesAPICaller.java
@@ -140,8 +140,6 @@ public class IssuesAPICaller extends ApiCaller {
             query.append(" AND project NOT IN (").append(config.getExcludedJiraProject()).append(")");
         }
 
-        query.append(" AND issuekey IN (SYSAD-34428, SYSAD-34496, SYSAD-34490)");
-
         return "jql=" + URLEncoder.encode(query.toString(), "UTF-8");
     }
 
