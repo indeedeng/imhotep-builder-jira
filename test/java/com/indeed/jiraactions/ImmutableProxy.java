7b1347728173ca2d1b2f52ba473dbfcfe68aef8a
Kevin Binswanger <kbinswanger@indeed.com>
2017-09-19 20:39:10 -0500

    Add some documentation

--- test/java/com/indeed/jiraactions/ImmutableProxy.java
+++ test/java/com/indeed/jiraactions/ImmutableProxy.java
@@ -11,6 +11,19 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Immutables are awesome, but they have one primary flaw: they can be hard to use in tests. We want the fields on the actual Action class
+ * to be nonnull. This helps detect errors early in testing (because it can be hard to validate the actual data is correct). However, the
+ * Action class changes frequently when new fields are created, and we don't want this to force someone to have to update tests for no value.
+ *
+ * This class attempts to solve this problem by taking advantage of the fact that you can build one Immutable by starting with another. This
+ * class basically helps you make mocks of Immutables. It returns a reasonable default value for all the types it knows about. So you can
+ * create a new Immutable, using this as the starting point. Therefore you end up with an Immutable that's like a nice mock; you can set it
+ * with whatever fields you want without having to do any more work.
+ *
+ * Sample usage:
+ * final Action action = ImmutableAction.builder().from(ImmutableProxy.createProxy(Action.class)).action("create").build();
+ *
+ * Whether I'm proud or ashamed to have written this depends on the day.
  * @author kbinswanger
  */
 public class ImmutableProxy<T> implements InvocationHandler {
@@ -49,7 +62,7 @@ public class ImmutableProxy<T> implements InvocationHandler {
         } else if (returnType.equals(float.class)) {
             return (float)0;
         } else if(returnType.equals(int.class)) {
-            return (int) 0;
+            return (int)0;
         } else if(returnType.equals(Boolean.class) || returnType.equals(boolean.class)) {
             return false;
         } else if(returnType.equals(String.class)) {
