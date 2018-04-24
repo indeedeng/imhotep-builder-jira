b1549c112ea367ddb093c359aae893eb06bf53a3
Kevin Binswanger <kbinswanger@indeed.com>
2018-04-24 17:40:22 -0500

    better error tolerance in link parsing

--- src/java/com/indeed/jiraactions/api/links/LinkFactory.java
+++ src/java/com/indeed/jiraactions/api/links/LinkFactory.java
@@ -1,9 +1,12 @@
 package com.indeed.jiraactions.api.links;
 
 import com.indeed.jiraactions.api.response.issue.changelog.histories.Item;
+import com.indeed.util.logging.Loggers;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,15 +14,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LinkFactory {
+    private static final Logger log = Logger.getLogger(LinkFactory.class);
     private static final Pattern LINK_PATTERN = Pattern.compile("This issue ([A-Za-z ]+) ([A-Z0-9]+-[0-9]+)");
 
-    protected Link makeLink(@Nonnull final String input) {
+    protected Link makeLink(@Nonnull final String input) throws IOException {
         final Matcher matcher = LINK_PATTERN.matcher(input);
         if(!matcher.matches()) {
-            throw new IllegalArgumentException("Failed to make link from " + input);
+            throw new IOException("Failed to make link from " + input);
         }
         if(matcher.groupCount() != 2) {
-            throw new IllegalArgumentException("Incorrect number of groups " + matcher.groupCount() + " from " + input);
+            throw new IOException("Incorrect number of groups " + matcher.groupCount() + " from " + input);
         }
 
         final String type = matcher.group(1);
@@ -34,12 +38,20 @@ public class LinkFactory {
         final Set<Link> output = new HashSet<>(source);
         for(final Item item : changes) {
             if(!StringUtils.isEmpty(item.fromString)) {
-                final Link link = makeLink(item.fromString);
-                output.remove(link);
+                try {
+                    final Link link = makeLink(item.fromString);
+                    output.remove(link);
+                } catch(final IOException e) {
+                    Loggers.error(log, "Error when creating Link to remove from '%s'", item.fromString);
+                }
             }
             if(!StringUtils.isEmpty(item.toString)) {
-                final Link link = makeLink(item.toString);
-                output.add(link);
+                 try {
+                     final Link link = makeLink(item.toString);
+                     output.add(link);
+                 } catch(final IOException e) {
+                     Loggers.error(log, "Error when creating Link to add from '%s'", item.toString);
+                 }
             }
         }
 
