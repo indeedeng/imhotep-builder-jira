196d82b122ec3f56a9efd0b5cfaac0c2c5642ff4
Kevin Binswanger <kbinswanger@indeed.com>
2020-06-25 22:48:29 -0500

    Clean up the test

--- src/test/java/com/indeed/jiraactions/ActionFactoryTest.java
+++ src/test/java/com/indeed/jiraactions/ActionFactoryTest.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Locale;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -33,23 +32,9 @@ public class ActionFactoryTest {
             final JsonNode node = new ObjectMapper().readTree(stream);
             final Issue issue = IssueAPIParser.getObject(node);
             final Action action = factory.create(issue);
+            Assert.assertEquals(new DateTime(2009, 2, 12, 17, 40, 27, DateTimeZone.forOffsetHours(-6)), action.getCreatedDate());
 
-            {
-                final DateTime actual = action.getCreatedDate();
-                final DateTime expected = new DateTime(2009, 2, 12, 17, 40, 27);
-                System.out.println(String.format("expected: \"%s\" \"%s\" \"%s\"", expected, expected.withZone(DateTimeZone.forOffsetHours(-6)), expected.withZone(DateTimeZone.forOffsetHours(-6)).toString(" yyyyMMdd'T'HHmmss.SSSZ", Locale.US)));
-                System.out.println(String.format("actual: \"%s\" \"%s\" \"%s\"", actual, actual.withZone(DateTimeZone.forOffsetHours(-6)), actual.withZone(DateTimeZone.forOffsetHours(-6)).toString(" yyyyMMdd'T'HHmmss.SSSZ", Locale.US)));
-            }
-            Assert.assertEquals(new DateTime(2009, 2, 12, 17, 40, 27, DateTimeZone.forOffsetHours(-6)).withZone(DateTimeZone.forOffsetHours(-6)).toString(" yyyyMMdd'T'HHmmss.SSSZ", Locale.US), action.getCreatedDate().toString(" yyyyMMdd'T'HHmmss.SSSZ", Locale.US));//Assert.assertEquals(20090212, action.getCreatedDateLong());
-            //Assert.assertEquals(20090212174027L, action.getCreatedDateTimeLong());
-            //Assert.assertEquals(1234482027, action.getCreatedDateTimestamp());
-            //org.joda.time.DateTime<2009-02-12T17:40:27.000-06:00>
-            //org.joda.time.DateTime<2009-02-12T17:40:27.000-06:00>
-
-            Assert.assertEquals(new DateTime(2009, 2, 12, 18, 48, 50, DateTimeZone.forOffsetHours(-6)).withZone(DateTimeZone.forOffsetHours(-6)).toString(" yyyyMMdd'T'HHmmss.SSSZ", Locale.US), action.getResolutionDate().get().toString(" yyyyMMdd'T'HHmmss.SSSZ", Locale.US));
-            //Assert.assertEquals(20090212, action.getResolutionDateLong());
-            //Assert.assertEquals(20090212184850L, action.getResolutionDateTimeLong());
-            //Assert.assertEquals(1234486130, action.getResolutionDateTimestamp());
+            Assert.assertEquals(new DateTime(2009, 2, 12, 18, 48, 50, DateTimeZone.forOffsetHours(-6)), action.getResolutionDate().get());
         }
     }
 
